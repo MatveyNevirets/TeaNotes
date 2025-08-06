49890b263d158cd91becebc29f003def0b12a9a2
Matvey <matvey.works@gmail.com>
2025-08-06 09:57:31 +0400

    Warm up screen as a step into the tea ceremony process done

--- lib/features/runtime_ceremony/presentation/warm_up_screen.dart
+++ lib/features/runtime_ceremony/presentation/warm_up_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tea_list/core/styles/app_colors.dart';
+import 'package:tea_list/core/widgets/stylized_button.dart';
 import 'package:tea_list/features/runtime_ceremony/presentation/bloc/ceremony_bloc.dart';
 
 class WarmUpScreen extends StatelessWidget {
@@ -11,20 +12,54 @@ class WarmUpScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          Positioned(
+            top: 64,
+            left: MediaQuery.of(context).size.width / 6,
+            right: MediaQuery.of(context).size.width / 6,
+            child: Text(
+              "Прогрейте посуду",
+              textAlign: TextAlign.left,
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontSize: 40, color: Colors.black.withAlpha(180)),
+            ),
+          ),
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
               color: AppColors.application3BaseColor,
               width: double.maxFinite,
-              height: MediaQuery.of(context).size.height / 3,
+              height: MediaQuery.of(context).size.height / 2.5,
             ),
           ),
-          Positioned(top: 20, child: Image.asset("assets/images/teapot_with_cups.png")),
           Align(
             alignment: Alignment.center,
-            child: ElevatedButton(
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 1),
+                    color: AppColors.applicationBaseColor,
+                    spreadRadius: 1,
+                    blurRadius: 3,
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(90),
+              ),
+              child: Image.asset(
+                "assets/images/teapot_with_cups.png",
+                height: MediaQuery.of(context).size.height / 2.5,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: MediaQuery.of(context).size.height / 15,
+            left: MediaQuery.of(context).size.width / 8,
+            right: MediaQuery.of(context).size.width / 8,
+            child: StylizedButton(
+              buttonSize: Size(double.maxFinite, MediaQuery.of(context).size.height / 12),
               onPressed: () => context.read<CeremonyBloc>().add(OnWarmedUpCeremonyEvent()),
-              child: Text("Готово"),
+              text: "Готово",
+              backgroundColor: AppColors.application2BaseColor,
+              textColor: Colors.black.withAlpha(180),
             ),
           ),
         ],
